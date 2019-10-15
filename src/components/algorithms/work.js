0531c2ea146361a8080fbf00b872c467a57aa661
Jude Okagu <juud0010@gmail.com>
2019-10-15 05:59:23 -0700

    new

--- src/components/algorithms/work.js
+++ src/components/algorithms/work.js
@@ -7,11 +7,11 @@ const work = (n) => {
     for (let i = 1; arr.length < n; i++) {
         
       if( i% 15 === 0) {
-        arr.push (' SKIM is a nice place to work!!, ')
+        arr.push (' DTT is a nice place to work!!, ')
       } else if (i % 5 === 0){
         arr.push(' nice place to work!!, ')
       } else if (i % 3 === 0){
-        arr.push(' SKIM, ')
+        arr.push(' DTT, ')
       } else {
         arr.push(` ${i}, `)
       }
