cd44fc29b89697c6a57ad2747bdd073785709b14
Jude Okagu <juud0010@gmail.com>
2020-02-04 23:10:21 +0100

    update

--- src/components/algorithms/work.js
+++ src/components/algorithms/work.js
@@ -7,7 +7,7 @@ const work = (n) => {
     for (let i = 1; arr.length < n; i++) {
         
       if( i% 15 === 0) {
-        arr.push ('  the best! , ')
+        arr.push ('  the just too beautiful! , ')
       } else if (i % 5 === 0){
         arr.push(' is, ')
       } else if (i % 3 === 0){
