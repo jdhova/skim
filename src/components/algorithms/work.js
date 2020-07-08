0c24081d93875a1cde0681c7bd4029dcc764958d
Jude Okagu <juud0010@gmail.com>
2020-07-08 20:43:05 +0200

    fix

--- src/components/algorithms/work.js
+++ src/components/algorithms/work.js
@@ -1,27 +1,19 @@
-
-
 const work = (n) => {
+  let arr = [];
 
-    let arr = []
-  
-    for (let i = 1; arr.length < n; i++) {
-        
-      if( i% 6 === 0) {
-        arr.push ('Latifah is the sexiest Angel with Beauty and Brains! , ')
-      } else if (i % 4 === 0){
-        arr.push(' is the sexiest Angel, ')
-      } else if (i % 2 === 0){
-        arr.push(' Latifah, ')
-      } else {
-        arr.push(` ${i}, `)
-      }
-    };
+  for (let i = 1; arr.length < n; i++) {
+    if (i % 6 === 0) {
+      arr.push('BINCOM is a nice place to work! , ');
+    } else if (i % 4 === 0) {
+      arr.push(' is a nice place to work, ');
+    } else if (i % 2 === 0) {
+      arr.push(' BINCOM, ');
+    } else {
+      arr.push(` ${i}, `);
+    }
+  }
 
-    return arr
-  
-  };
-  
+  return arr;
+};
 
 export default work;
-
-
