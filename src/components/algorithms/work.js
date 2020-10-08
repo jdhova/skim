7b8e838daf153187bbf39b232482ec8211d14d4b
Jude Okagu <juud0010@gmail.com>
2020-10-08 22:22:32 +0200

    jj

--- src/components/algorithms/work.js
+++ src/components/algorithms/work.js
@@ -3,11 +3,11 @@ const work = (n) => {
 
   for (let i = 1; arr.length < n; i++) {
     if (i % 6 === 0) {
-      arr.push('BINCOM is a nice place to work! , ');
+      arr.push('JUUD is a nice place to work! , ');
     } else if (i % 4 === 0) {
       arr.push(' is a nice place to work, ');
     } else if (i % 2 === 0) {
-      arr.push(' BINCOM, ');
+      arr.push(' JUUD, ');
     } else {
       arr.push(` ${i}, `);
     }
--- src/components/input.js
+++ src/components/input.js
@@ -1,54 +1,69 @@
-
 import React, { Component } from 'react';
 import work from './algorithms/work';
 
 class input extends Component {
-    constructor(props){
-        super(props)
-            this.state = {
-                value : '',
-                finalValue :''          
-            }
-        };
-
-        handleChange = (e) => {
-            this.setState({value: (e.target.value)})     
-        };
-
-        onSubmit = (e) => {
-            e.preventDefault()
-            this.setState ({ finalValue: this.state.value})
-            this.setState({value: ''})        
-        };
-
-        onReset = (e) => {
-            e.preventDefault()
-            this.setState({finalValue : ''})
-        };
-
-        render() { 
-                const strval = this.state.finalValue;
-                const intval = parseInt(strval); 
-
-            return (
-              <div className = 'input'>    
-                <form className="form-inline">
-                    <div className="form-group mx-sm-3 mb-2">
-                        <label className="sr-only">Enter integer</label>
-                        <input type="number" className="form-control" value = {this.state.value} onChange = {this.handleChange} placeholder="Enter integer" />
-                    </div>
-                        <button type="submit" onClick = {this.onSubmit} className="btn btn-success mb-2">Submit</button>
-                </form>
-                
-                    <h2 className = "wrk"> {work(intval)}</h2>
-
-                    <button type= "submit" onClick = {this.onReset} className="btn btn-primary mb-2">Reset</button>
-              </div>
-                
-            )
-          };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      finalValue: '',
     };
+  }
 
-  
-export default  input;
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ finalValue: this.state.value });
+    this.setState({ value: '' });
+  };
+
+  onReset = (e) => {
+    e.preventDefault();
+    this.setState({ finalValue: '' });
+  };
+
+  render() {
+    const strval = this.state.finalValue;
+    const intval = parseInt(strval);
+
+    return (
+      <div className='input'>
+        <form className='form-inline'>
+          <div className='form-group mx-sm-3 mb-2'>
+            <label className='sr-only'>Enter integer</label>
+            <input
+              type='number'
+              className='form-control'
+              value={this.state.value}
+              onChange={this.handleChange}
+              placeholder='Enter integer'
+            />
+          </div>
+          <button
+            type='submit'
+            // type='submit'
+            onClick={this.onSubmit}
+            className='btn btn-success mb-2'
+          >
+            Submit
+          </button>
+        </form>
+
+        <h2 className='wrk'> {work(intval)}</h2>
+
+        <button
+          type='submit'
+          onClick={this.onReset}
+          className='btn btn-primary mb-2'
+        >
+          Reset
+        </button>
+      </div>
+    );
+  }
+}
+
+export default input;
