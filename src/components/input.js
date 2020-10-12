383b1ab12296fe01e75257d30eb4d1f716a438da
Jude Okagu <juud0010@gmail.com>
2020-10-12 13:13:52 +0200

    fix

--- src/components/input.js
+++ src/components/input.js
@@ -40,7 +40,6 @@ class input extends Component {
               value={this.state.value}
               onChange={this.handleChange}
               placeholder='Enter integer'
-              // placeholder='Enter integer'
             />
           </div>
           <button
